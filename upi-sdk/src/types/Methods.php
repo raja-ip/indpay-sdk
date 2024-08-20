e145b9ed97602076b36126c896004e1b40fa00e0
Dharani <dharanitharan@indusspay.com>
2024-08-20 05:05:34 +0000

    re-commit with folder restructure

--- upi-sdk/src/types/Methods.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-enum Methods {
-    case GET;
-    case POST;
-    case PUT;
-    case PATCH;
-    case DELETE;
-}
--- upi-sdk/src/utils/Credentials.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-function createCredentials($username, $password) {
-    $usernameAndPassword = "{$username}:{$password}";
-    $encoded = base64_encode($usernameAndPassword);
-    return "Basic {$encoded}";
-}
